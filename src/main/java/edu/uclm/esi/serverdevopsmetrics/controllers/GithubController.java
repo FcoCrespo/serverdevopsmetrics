4871e19a7f77823d9721ae89c2fa49718ed073db
FcoCrespo <Fco.Crespo@alu.uclm.es>
2021-06-04 00:35:46 +0200

    warnings

--- src/main/java/edu/uclm/esi/serverdevopsmetrics/controllers/GithubController.java
+++ src/main/java/edu/uclm/esi/serverdevopsmetrics/controllers/GithubController.java
@@ -2,8 +2,6 @@ package edu.uclm.esi.serverdevopsmetrics.controllers;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.Scanner;
 
